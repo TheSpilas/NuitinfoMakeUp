8e1e5aaa702c83ab4773da4d4859c59a9b9e1432
Victor Murris <murrisvictor@gmail.com>
2024-12-06 05:01:41 +0100

    game est integre

--- OceanHospital/actions/auth/securityAction.php
+++ OceanHospital/actions/auth/securityAction.php
@@ -1,5 +1,11 @@
 <?php
-session_start();
-if (!isset($_SESSION['auth'])){
-    header('Location: login.php');
+// Vérifiez si une session est déjà active avant de démarrer une nouvelle session
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
 }
+
+if (!isset($_SESSION['auth'])) {
+    header('Location: login.php');
+    exit; // Ajout d'un exit pour s'assurer que le script s'arrête après la redirection
+}
+?>
