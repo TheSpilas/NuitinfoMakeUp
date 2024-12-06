51a26ec379fd85ad50357fd8924614048d50c8b8
Victor Murris <murrisvictor@gmail.com>
2024-12-06 04:30:44 +0100

    morpion finiiiiii

--- OceanHospital/login.php
+++ OceanHospital/login.php
@@ -1,12 +1,17 @@
-<?php require('actions/auth/loginAction.php'); ?>
+<?php require('actions/auth/loginAction.php');?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Hospital Ocean</title>
+    <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/styles/styles.css">
+    <style>
+        .hidden {
+            display: none;
+        }
+    </style>
 </head>
 <body class="d-flex flex-column min-vh-100">
 <header style="background-color: var(--header-color);" class="text-white py-3 text-center">
@@ -14,8 +19,30 @@
     <h1>Hospital Ocean</h1>
     <p class="motto">Heal the Ocean</p>
 </header>
-<main class="flex-grow-1 d-flex align-items-center justify-content-center">
-    <div class="login-form-container p-4 rounded shadow">
+<!-- Jeu de morpion CAPTCHA -->
+<div id="game-board" class="about mb-4">
+       <h2>Jeu de Morpion</h2>
+        <table id="morpion" border="1" style="width: 300px; height: 300px;">
+            <tr>
+                <td data-cell="0" style="width: 33.33%; height: 33.33%;"></td>
+                <td data-cell="1" style="width: 33.33%; height: 33.33%;"></td>
+                <td data-cell="2" style="width: 33.33%; height: 33.33%;"></td>
+            </tr>
+            <tr>
+                <td data-cell="3" style="width: 33.33%; height: 33.33%;"></td>
+                <td data-cell="4" style="width: 33.33%; height: 33.33%;"></td>
+                <td data-cell="5" style="width: 33.33%; height: 33.33%;"></td>
+            </tr>
+            <tr>
+                <td data-cell="6" style="width: 33.33%; height: 33.33%;"></td>
+                <td data-cell="7" style="width: 33.33%; height: 33.33%;"></td>
+                <td data-cell="8" style="width: 33.33%; height: 33.33%;"></td>
+            </tr>
+        </table>
+     <div id="message">C'est au tour de X !</div>
+ </div>
+ <main id="login-form" class="flex-grow-1 d-flex align-items-center justify-content-center hidden">
+<div   class=" login-form-container p-4 rounded shadow">
         <h2 class="text-center mb-4">Connexion</h2>
         <form method="POST">
             <?php if (isset($errorMsg)) { echo '<p class="text-danger text-center">' . $errorMsg . '</p>'; } ?>
@@ -38,6 +65,6 @@
 <footer class="bg-dark text-white text-center py-3">
     <p>Made by TurboMakeup</p>
 </footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+<script src="assets/scripts/captcha1.js"></script>
 </body>
 </html>
